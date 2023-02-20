513352e29b11454b279b71c5c1f83a7d9e80f4a9
kartik01a <93159567+kartik01a@users.noreply.github.com>
2023-02-21 00:47:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from_whatsapp_no = os.environ['from_whatsapp_no']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='Code Pushed in master branch of https://github.com/kartik01a/github-to-whatsapp-push-message-notifier',
+                              body='Code Pushed in main2 branch of https://github.com/kartik01a/github-to-whatsapp-push-message-notifier',
                               from_='whatsapp:'+from_whatsapp_no,
                               to='whatsapp:'+to_whatsapp_no
                           )
